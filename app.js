608fa7cced1bc808584dae627a0c0a071e024b78
amarchinta1 <amar.chinta@zerebral.co.in>
2019-08-12 16:16:55 +0530

    all files are added ...

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//To  command 
+//INPUT=./sample.sh OUTPUT=./out.txt node app.js
+
+
 const csvjson = require('csvjson');
 const readFile = require('fs').readFile;
 const writeFile = require('fs').writeFile;
 const output = process.env.OUTPUT
 const input = process.env.INPUT
+console.log(input);
+console.log(output);
+
 var child_process= require('child_process').exec;
 
 if (!input) {
@@ -10,7 +17,7 @@ if (!input) {
     process.exit(0);
 }
 
-child_process(input,{ maxBuffer : 1024 * 1024}, function(err,stdout){
+child_process(input,{ maxBuffer : 5000 * 5000}, function(err,stdout){
 
 if(err)
 {
